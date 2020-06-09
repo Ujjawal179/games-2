859817c4bc8cbc2fd9d85428ceffcdb94275c1dd
samshrita <58076181+samshrita@users.noreply.github.com>
2020-06-09 20:48:58 +0545

    Add files via upload
    It is word guessing game. As per the options present it takes random word form code and then ask user to enter a letter,  checks if it is correct , and prints if correct else educes option to guess.

---
+++ wordguessgame.py
@@ -0,0 +1,58 @@
+import random 
+  
+name = input("What is your name? ") 
+  
+print("Good Luck ! ", name) 
+  
+words = ['remainder', 'computer', 'science', 'programming',  
+         'python', 'mathematics','engineerinng', 'player', 'condition',  
+         'reverse', 'water', 'board', 'kitty', 'hydrogen' , 'complex', 'keratine','kreatimine', 'kepler']  
+  
+word = random.choice(words) 
+  
+  
+print("Guess the characters") 
+  
+guesses = '' 
+  
+turns = 12
+  
+  
+while turns > 0: 
+    failed = 0
+      
+    for char in word:  
+          
+        if char in guesses:  
+            print(char) 
+              
+        else:  
+            print("_") 
+              
+            failed += 1
+              
+  
+    if failed == 0: 
+        print("You Win")  
+          
+        print("The word is: ", word)  
+        break
+      
+    guess = input("guess a character:") 
+      
+    
+    guesses += guess  
+      
+ 
+    if guess not in word: 
+          
+        turns -= 1
+           
+        print("Wrong") 
+          
+         
+        print("You have", + turns, 'more guesses left') 
+          
+          
+        if turns == 0: 
+            print("You Loose!bettter luck next time")
